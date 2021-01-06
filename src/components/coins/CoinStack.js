20e1052ecfceea8a6f970d9acee77ae51743a4c2
Alexis Borges <tuaalexis@gmail.com>
2021-01-06 09:18:01 -0600

    Inicializacion crypto tracker

---
+++ src/components/coins/CoinStack.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const CoinsStack = () =>{ 
+    return (
+
+
+    );
+
+}
+
+export default CoinsStack;
